0000001036cab3eb936daf0383d6557025072e56
MCSeekeri <mcseekeri@outlook.com>
2024-12-07 10:07:04 +0000

    feat: 加入 Flake 和 Home Manager

--- configuration.nix
+++ configuration.nix
@@ -19,7 +19,23 @@
 
   # 设置软件源
   nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d"; # 删除超过一周的垃圾文件，硬盘笑传之踩踩 Backup
+    };
     settings = {
+      # auto-optimise-store = true; # 会让 build 变慢，见仁见智吧
+      substituters = lib.mkForce [
+        "https://mirrors.ustc.edu.cn/nix-channels/store"
+        "https://mirror.sjtu.edu.cn/nix-channels/store"
+        "https://mirror.nju.edu.cn/nix-channels/store"
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
       # 启动 Flake，勿动，除非你知道你在做什么
       # https://nixos.wiki/wiki/Flakes
       experimental-features = [
