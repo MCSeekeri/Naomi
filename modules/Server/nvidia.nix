0000018872936e24c362f79c6d0cbf02cacf094e
MCSeekeri <mcseekeri@outlook.com>
2024-12-22 12:34:44 +0000

    fix: 修复主机配置，微调 Nvidia 设置

--- modules/Server/nvidia.nix
+++ modules/Server/nvidia.nix
@@ -2,9 +2,17 @@
   hardware = {
     nvidia-container-toolkit.enable = true;
     nvidia = {
+      open = true;
       modesetting.enable = true;
       nvidiaPersistenced = true;
-      datacenter.enable = true;
+      datacenter.enable = false;
     };
+  };
+  services.xserver = {
+    videoDrivers = [
+      "nvidia"
+      "modesetting"
+      "fbdev"
+    ];
   };
 }
