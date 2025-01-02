00000250a62be373487ff95f63da203de13cd99d
MCSeekeri <mcseekeri@outlook.com>
2025-01-02 14:43:27 +0000

    refactor: Podman 配置拆分

---
+++ modules/Core/podman.nix
@@ -0,0 +1,20 @@
+{
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+      dockerSocket.enable = true;
+      dockerCompat = true; # 用户体验不变
+      networkSocket.openFirewall = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
+    # oci-containers.containers = {
+    #   hello = {
+    #     image = "docker.io/library/hello-world:latest";
+    #     autoStart = false;
+    #     ports = [ "" ];
+    #   };
+    # };
+    # 我之后修好这个
+  };
+}
