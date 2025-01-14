000003422ee68dd52b22b9734b9aca27d916dda3
MCSeekeri <mcseekeri@outlook.com>
2025-01-14 05:09:44 +0000

    refactor: 用户加密方式修改

--- users/mcseekeri/default.nix
+++ users/mcseekeri/default.nix
@@ -1,9 +1,9 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 {
-  sops.secrets.mcseekeri-pwd = {
-    sopsFile = ../../secrets/users.yaml;
-    neededForUsers = true;
-  };
+  # sops.secrets.mcseekeri-pwd = {
+  #   sopsFile = ../../secrets/users.yaml;
+  #   neededForUsers = true;
+  # };
   users.users.mcseekeri = {
     isNormalUser = true;
     #description = "";
@@ -18,7 +18,8 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOTidxwTS5kyQENgBQ1n4FukaocS1CHhBZ0uaEDifLA0 mcseekeri@outlook.com"
     ];
-    hashedPasswordFile = config.sops.secrets.mcseekeri-pwd.path;
+    hashedPassword = "$y$j9T$lxgYXhMXDKlSzeS55r.nT/$X0TR61QgiVXNMYpbHOXitEKrtYE7LvKQf/gYgcL3Nc0";
+    # hashedPasswordFile = config.sops.secrets.mcseekeri-pwd.path;
   };
   home-manager.users.mcseekeri = import ./home-manager.nix;
 }
