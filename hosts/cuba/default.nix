000004107f2f50ac29cd0bf5e8ecbda0f4c41caa
MCSeekeri <mcseekeri@outlook.com>
2025-01-22 11:29:04 +0000

    feat: 改进定制系统镜像

--- hosts/cuba/default.nix
+++ hosts/cuba/default.nix
@@ -19,11 +19,55 @@
     autoUpgrade.enable = true;
   };
 
+  boot.tmp = {
+    # 很冒险的设置，等待后续测试
+    useTmpfs = true;
+    tmpfsSize = "75%";
+  };
+
+  nix = {
+    settings = {
+      substituters = lib.mkForce [
+        "https://mirrors.ustc.edu.cn/nix-channels/store?priority=1"
+        "https://mirror.sjtu.edu.cn/nix-channels/store?priority=2"
+        "https://cache.nixos.org/?priority=3"
+        "https://cache.garnix.io?priority=4"
+        "https://nix-community.cachix.org?priority=5"
+        "https://numtide.cachix.org?priority=6"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      ];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      trusted-users = [ "root" ];
+      connect-timeout = 20;
+      http-connections = 64;
+      max-substitution-jobs = 32; # 加速下载
+      max-free = 5 * 1024 * 1024 * 1024;
+      min-free = 1024 * 1024 * 1024;
+      builders-use-substitutes = true;
+    };
+  };
+
   users.users.root = {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOTidxwTS5kyQENgBQ1n4FukaocS1CHhBZ0uaEDifLA0 mcseekeri@outlook.com"
     ];
     # hashedPassword = "";
+  };
+  systemd = {
+    # 以防万一
+    targets = {
+      sleep.enable = false;
+      suspend.enable = false;
+      hibernate.enable = false;
+      hybrid-sleep.enable = false;
+    };
   };
 
 }
