000003533884cf7e2f25a6e425005b2415fe6b8f
MCSeekeri <mcseekeri@outlook.com>
2025-01-22 03:33:10 +0000

    feat: 添加新设备

--- docs/designator.md
+++ docs/designator.md
@@ -2,6 +2,7 @@
 - `manhattan`: Oracle VirtualBox 虚拟机，目前主要的测试对象。
 - `cyprus`: Lenovo Y7000P，用于开发的笔记本，同时也是主要游戏设备。
 - `seychelles`: Lenovo ThinkStation PX，用于模型训练的工作站。
+- `costarica`: VMware 虚拟机，用于搭建 Minecraft 服务器。
 - `cuba`: 用于生成 Live CD 的配置，目前仅用于测试。
 ## 兼容性问题
 目前 Naomi 仍在测试阶段，`cyprus` 和 `seychelles` 仍然属于"虚空开发"状态，只有 `manhattan` 可用。\
---
+++ hosts/costarica/default.nix
@@ -0,0 +1,29 @@
+{ ... }:
+{
+  imports = [
+    ./disko-config.nix
+
+    ../../modules/Core
+
+    ../../modules/Server/firewall.nix
+    ../../modules/Server/hardened.nix
+    ../../modules/Desktop/gui.nix
+    ../../modules/Desktop/programs.nix
+    ../../modules/Desktop/fcitx5.nix
+    ../../modules/Games/minecraft_server.nix
+
+    ../../users/mcseekeri
+  ];
+
+  # 网络配置
+  networking = {
+    hostName = "costarica"; # 主机名，设置好之后最好不要修改
+  };
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  system = {
+    stateVersion = "24.11";
+    autoUpgrade.enable = true;
+  };
+  virtualisation.vmware.guest.enable = true;
+}
