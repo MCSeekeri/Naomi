0000065750a8285d6a014512403a177bb5b00658
MCSeekeri <mcseekeri@outlook.com>
2025-02-23 16:38:41 +0000

    feat(modules/Services): 实验性加入 AdGuardHome 配置

---
+++ modules/Services/adguardhome.nix
@@ -0,0 +1,51 @@
+{
+  services.adguardhome = {
+    enable = true;
+    settings = {
+      dns = {
+        upstream_dns = [
+          "https://doh.pub/dns-query"
+          "https://cloudflare-dns.com/dns-query"
+        ];
+        bootstrap_dns = [
+          "119.29.29.29"
+          "119.28.28.28"
+          "223.5.5.5"
+          "223.6.6.6"
+          "1.1.1.1"
+          "1.0.0.1"
+          "2402:4e00::"
+          "2400:3200::1"
+          "2606:4700:4700::1111"
+        ];
+      };
+      filters = [
+        {
+          enabled = true;
+          url = "https://gcore.jsdelivr.net/gh/TG-Twilight/AWAvenue-Ads-Rule@main/AWAvenue-Ads-Rule.txt";
+          name = "AWAvenue Ads Rule";
+          id = 114;
+        }
+        {
+          enabled = true;
+          url = "https://gcore.jsdelivr.net/gh/1Hosts/1Hosts@master/hosts.txt";
+          name = "1Hosts (mini)";
+          id = 514;
+        }
+        {
+          enabled = true;
+          url = "https://gp.adrules.top/dns.txt";
+          name = "CHN=AdRules DNS List";
+          id = 1919;
+        }
+        {
+          enabled = true;
+          url = "https://anti-ad.net/easylist.txt";
+          name = "CHN=anti-AD";
+          id = 810;
+        }
+      ];
+    };
+
+  };
+}
