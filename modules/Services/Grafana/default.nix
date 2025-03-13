00000750bcf2dba8d0186d98a44b93175b4286d6
MCSeekeri <mcseekeri@outlook.com>
2025-03-13 12:57:41 +0000

    feat: 实验性加入 Grafana

---
+++ modules/Services/Grafana/default.nix
@@ -0,0 +1,34 @@
+{
+  systemd.services.grafana = {
+    after = [ "network.target" ];
+    wants = [ "network.target" ];
+  };
+  services.grafana = {
+    enable = true;
+    settings = {
+      server = {
+        http_addr = "127.0.0.1";
+        http_port = 4300;
+        domain = "127.0.0.1";
+        addr = "";
+        serve_from_sub_path = true;
+      };
+    };
+    provision.enable = true;
+  };
+  services.prometheus = {
+    enable = true;
+    port = 4301;
+    exporters = {
+      node = {
+        enable = true;
+        enabledCollectors = [ "systemd" ];
+        port = 9002;
+      };
+    };
+  };
+  services.loki = {
+    enable = true;
+    configFile = ./loki-config.yml;
+  };
+}
