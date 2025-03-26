000010206974d1a39bebaca4617417325d8b5ce9
MCSeekeri <mcseekeri@outlook.com>
2025-03-26 11:46:58 +0000

    feat: tailscale 自动上线

--- modules/Core/tailscale.nix
+++ modules/Core/tailscale.nix
@@ -1,9 +1,15 @@
+{ config, ... }:
 {
   services = {
     tailscale = {
       enable = true;
-      extraUpFlags = [ "--accept-routes --advertise-exit-node" ];
+      extraUpFlags = [ "--advertise-exit-node" ];
       openFirewall = true;
+      authKeyFile = "${config.sops.secrets."auth-key".path}";
     };
+  };
+  # 90 天一换，别忘记了……
+  sops.secrets."auth-key" = {
+    sopsFile = ../../secrets/services/tailscale.yaml;
   };
 }
