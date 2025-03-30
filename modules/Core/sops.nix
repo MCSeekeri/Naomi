00001083e92d40a09aef8aaa92d3f863c8c5faf6
MCSeekeri <mcseekeri@outlook.com>
2025-03-30 09:11:28 +0000

    style: Prettier

--- modules/Core/sops.nix
+++ modules/Core/sops.nix
@@ -3,7 +3,7 @@
   imports = [ inputs.sops-nix.nixosModules.sops ];
   sops.age = {
     # 设备部署之后，反过来复制密钥，然后添加到 Github Repo
-    # TODO: 未来优化一下这个流程
+    # [TODO]: 未来优化一下这个流程
     keyFile = "/var/lib/sops-nix/key.txt";
     generateKey = true;
   };
