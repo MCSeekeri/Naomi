0000116065753e29eed8a7c1c2afd9ae24ea0d2f
MCSeekeri <mcseekeri@outlook.com>
2025-04-03 03:28:15 +0000

    refactor: 移除 devenv

--- flake.nix
+++ flake.nix
@@ -18,13 +18,11 @@
       "https://cache.garnix.io?priority=4"
       "https://nix-community.cachix.org?priority=5"
       "https://numtide.cachix.org?priority=6"
-      "https://devenv.cachix.org?priority=7"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     ];
   };
 
@@ -99,10 +97,6 @@
       url = "github:nix-community/nix-vscode-extensions";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    devenv = {
-      url = "github:cachix/devenv";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
   };
 
@@ -111,7 +105,6 @@
       self,
       nixpkgs,
       nix-topology,
-      devenv,
       nixos-generators,
       ...
     }@inputs:
@@ -183,9 +176,25 @@
           specialArgs = { inherit self inputs; };
         };
       };
-      devShells.x86_64-linux.default = devenv.lib.mkShell {
-        inherit inputs pkgs;
-        modules = [ ./devenv.nix ];
+
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nix
+          git
+          sops
+          age
+          home-manager
+          nix-init
+          nh
+          nixfmt-rfc-style
+          fh
+          libressl # openssl rand -hex 64
+          deadnix
+        ];
+
+        shellHook = ''
+          export FLAKE="."
+        '';
       };
     };
 }
