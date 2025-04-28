000013100096754e3cf83674ed3fcc95556b22ae
MCSeekeri <mcseekeri@outlook.com>
2025-04-28 08:30:36 +0000

    refactor(pkgs): 完善 geph5-client 配置

--- pkgs/geph5-client/default.nix
+++ pkgs/geph5-client/default.nix
@@ -5,11 +5,22 @@
   pkg-config,
   libxkbcommon,
   openssl,
-  jemalloc,
+  rust-jemalloc-sys-unprefixed,
   sqlite,
   vulkan-loader,
   wayland,
+  iproute2,
+  iptables,
+  libglvnd,
+  copyDesktopItems,
+  makeDesktopItem,
 }:
+let
+  binPath = lib.makeBinPath [
+    iproute2
+    iptables
+  ];
+in
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "geph5";
   version = "0.2.43";
@@ -22,35 +33,75 @@ rustPlatform.buildRustPackage (finalAttrs: {
   };
 
   cargoHash = "sha256-lpWKzsgMSRg3BNdCyXn6b9HdGjndZF8YOoTIshz4BYU=";
+  postPatch = ''
+    substituteInPlace binaries/geph5-client/src/vpn/*.sh \
+      --replace-fail 'PATH=' 'PATH=${binPath}:'
+  '';
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [
+    pkg-config
+    copyDesktopItems
+  ];
 
   buildInputs = [
-    libxkbcommon
     openssl
-    jemalloc
+    rust-jemalloc-sys-unprefixed
     sqlite
-    vulkan-loader
-    wayland
   ];
-
-  cargoBuildFlags = [
-    "--package"
-    "geph5-client"
-  ];
-  doCheck = false;
 
   env = {
     OPENSSL_NO_VENDOR = true;
   };
+
+  buildFeatures = [ "windivert" ];
+
+  checkFlags = [
+    # Wrong test
+    "--skip=traffcount::tests::test_traffic_cleanup"
+    # Requires network
+    "--skip=dns::tests::resolve_google"
+    # Never finish
+    "--skip=tests::test_blind_sign"
+    "--skip=tests::test_generate_secret_key"
+  ];
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = "Geph5";
+      desktopName = "Geph5";
+      icon = "geph5";
+      exec = "geph5-client-gui";
+      categories = [ "Network" ];
+      comment = "Modular Internet censorship circumvention system designed specifically to deal with national filtering";
+    })
+  ];
+
+  postInstall = ''
+    install -m 444 -D binaries/geph5-client-gui/icon.png $out/share/icons/hicolor/512x512/apps/geph5.png
+  '';
+
+  postFixup = ''
+    # Add required but not explicitly requested libraries
+    patchelf --add-rpath '${
+      lib.makeLibraryPath [
+        wayland
+        libxkbcommon
+        vulkan-loader
+        libglvnd
+      ]
+    }' "$out/bin/geph5-client-gui"
+  '';
 
   meta = {
     description = "Modular Internet censorship circumvention system designed specifically to deal with national filtering";
     homepage = "https://github.com/geph-official/geph5";
     changelog = "https://github.com/geph-official/geph5/releases/tag/geph5-client-v${finalAttrs.version}";
     mainProgram = "geph5-client";
-    platforms = lib.platforms.linux;
+    platforms = lib.platforms.unix;
     license = lib.licenses.mpl20;
-    maintainers = with lib.maintainers; [ MCSeekeri ];
+    maintainers = with lib.maintainers; [
+      penalty1083
+      MCSeekeri
+    ];
   };
 })
