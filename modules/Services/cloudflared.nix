00001690eb409cb5a87b6a5b5f39005579741fd0
MCSeekeri <mcseekeri@outlook.com>
2025-06-21 06:57:00 +0000

    chore: 移除过时内容

--- modules/Services/cloudflared.nix
+++ modules/Services/cloudflared.nix
@@ -12,8 +12,6 @@
   # 虽然虽然官方仪表板配置的方式不那么"Nix 风味"，但是我个人认为隧道这种配置最好还是能够随时调整，适应不同情况。
   # 记得在启动之后迁移隧道，理论上不会影响现有功能。
 
-  # [TODO] 25.05 版本使用动态用户，不再固定为 cloudflared，需要测试
-
   sops.secrets."cloudflare-tunnel-${config.networking.hostName}" = {
     sopsFile = "${self}/secrets/services/cloudflare.yaml";
   };
