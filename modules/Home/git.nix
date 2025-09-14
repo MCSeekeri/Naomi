00002050cca3345d2ca26b982f17d2ece43c3141
MCSeekeri <mcseekeri@outlook.com>
2025-09-14 17:39:58 +0800

    feat: 加入 Git 的配置

---
+++ modules/Home/git.nix
@@ -0,0 +1,19 @@
+{
+  programs.git = {
+    enable = true;
+    extraConfig = {
+      push.autoSetupRemote = true;
+      pull.rebase = true;
+      log.date = "iso"; # 显而易见
+      submodule.recurse = true; # 自动拉取子模块
+    };
+    delta = {
+      enable = true;
+      options = {
+        diff-so-fancy = true;
+        line-numbers = true;
+        true-color = "always";
+      };
+    };
+  };
+}
