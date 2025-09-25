000022300dfccb5b44e88891a6321a95dffeb80a
MCSeekeri <mcseekeri@outlook.com>
2025-09-25 11:57:46 +0800

    feat: 实验性引入 STM32CubeIDE

--- modules/Desktop/embedded.nix
+++ modules/Desktop/embedded.nix
@@ -1,5 +1,7 @@
 { pkgs, ... }:
 {
+  # 谨以此配置文件，献给精通嵌入式开发的天才变态冒失娘二游痴美少女
+  # 这人哪哪都好，可惜沾上粥和少前了……
   environment.systemPackages = with pkgs; [
     arduino
     arduino-cli
@@ -19,6 +21,8 @@
     stm32cubemx
     stm32flash
     stm32loader
+    stm32cubeide
+    # nix-store --add-fixed sha256 st-stm32cubeide_1.19.0_25607_20250703_0907_amd64.sh.zip
 
     gcc-arm-embedded # arm-none-eabi-gcc
     openocd
@@ -38,4 +42,8 @@
   ];
 
   services.flatpak.packages = [ "cn.lceda.LCEDAPro" ];
+
+  environment.sessionVariables = {
+    WEBKIT_DISABLE_COMPOSITING_MODE = "1"; # 日食的黑色主题需要
+  };
 }
