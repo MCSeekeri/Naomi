0000231b3a1751a50c42268d6dc8db204de22b55
MCSeekeri <mcseekeri@outlook.com>
2025-10-09 18:57:35 +0800

    refactor: 在 NVIDIA 笔记本上优先启用卸载模式

--- modules/Hardware/nvidia_laptop.nix
+++ modules/Hardware/nvidia_laptop.nix
@@ -5,30 +5,35 @@
 
   hardware.nvidia = {
     prime = {
-      sync.enable = true;
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      sync.enable = lib.mkDefault false;
+
       intelBusId = lib.mkDefault "PCI:0:2:0";
       nvidiaBusId = lib.mkDefault "PCI:1:0:0"; # https://wiki.nixos.org/wiki/Nvidia
     };
     # 主机配置里写比较合适
   };
-  specialisation.offload.configuration = {
-    system.nixos.tags = [ "offload" ];
-    services.xserver.videoDrivers = [
-      "modesetting"
-      "nvidia"
-    ];
-    hardware.nvidia = {
-      powerManagement = {
-        enable = true;
-        finegrained = true;
-      };
-      prime = {
-        offload = {
-          enable = lib.mkForce true;
-          enableOffloadCmd = lib.mkForce true;
-        };
-        sync.enable = lib.mkForce false;
-      };
-    };
-  };
+  # specialisation.offload.configuration = {
+  #   system.nixos.tags = [ "sync" ];
+  #   services.xserver.videoDrivers = [
+  #     "modesetting"
+  #     "nvidia"
+  #   ];
+  #   hardware.nvidia = {
+  #     powerManagement = {
+  #       enable = true;
+  #       finegrained = true;
+  #     };
+  #     prime = {
+  #       sync.enable = true;
+  #       offload = {
+  #         enable = lib.mkForce false;
+  #         enableOffloadCmd = lib.mkForce false;
+  #       };
+  #     };
+  #   };
+  # };
 }
