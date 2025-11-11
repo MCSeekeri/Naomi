0000250d32738d1ff9474b0754b146b613fc2cfc
MCSeekeri <mcseekeri@outlook.com>
2025-11-11 16:23:15 +0800

    fix: 修复 geph5 CLI 的性能问题

--- hosts/cyprus/default.nix
+++ hosts/cyprus/default.nix
@@ -24,7 +24,7 @@
 
     "${self}/modules/Services/archisteamfarm.nix"
     "${self}/modules/Services/dae"
-    # "${self}/modules/Services/geph5.nix"
+    "${self}/modules/Services/geph5.nix"
     "${self}/modules/Services/localsend.nix"
 
     "${self}/modules/Games/retro.nix"
