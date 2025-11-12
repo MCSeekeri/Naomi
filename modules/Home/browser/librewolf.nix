0000255c1b6c083fa904d8380d919759c6ed2496
MCSeekeri <mcseekeri@outlook.com>
2025-11-12 22:24:57 +0800

    feat: 增加必备扩展

--- modules/Home/browser/librewolf.nix
+++ modules/Home/browser/librewolf.nix
@@ -83,6 +83,7 @@
               violentmonkey
               multi-account-containers
               temporary-containers-plus
+              bilisponsorblock
             ];
           };
         };
