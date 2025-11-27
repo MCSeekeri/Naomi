0000258807d030291a9340e72e7660d2d01be5a9
MCSeekeri <mcseekeri@outlook.com>
2025-11-27 15:15:27 +0800

    feat(modules): 硬件与打印机相关配置完善

--- modules/Desktop/yubikey.nix
+++ modules/Desktop/yubikey.nix
@@ -14,7 +14,14 @@
       };
     };
   };
-  services.udev.packages = [ pkgs.yubikey-personalization ];
+
+  services = {
+    yubikey-agent.enable = true;
+  };
+
+  hardware.gpgSmartcards.enable = true;
+  boot.initrd.luks.yubikeySupport = true; # [TODO] 测试能否使用 YubiKey 解锁 LUKS，而不是模拟键盘输入密码……
+  programs.yubikey-touch-detector.enable = true;
   environment.systemPackages = with pkgs; [
     yubioath-flutter
     yubikey-manager
