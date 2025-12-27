0000273e9441b2b6fb654cb75f38b2257f947c1a
MCSeekeri <mcseekeri@outlook.com>
2025-12-27 18:15:55 +0800

    refactor: 从 Lix 切换回 Nix

--- flake.nix
+++ flake.nix
@@ -7,20 +7,18 @@
       "flakes"
     ];
     extra-substituters = [
-      "https://mirrors.ustc.edu.cn/nix-channels/store?priority=1"
+      "https://mirrors.cernet.edu.cn/nix-channels/store?priority=1"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store?priority=2"
-      "https://cache.nixos.org?priority=3"
+      "https://mirror.sjtu.edu.cn/nix-channels/store?priority=3"
       "https://nix-community.cachix.org?priority=4"
       "https://numtide.cachix.org?priority=5"
       "https://cache.garnix.io?priority=6"
-      "https://cache.lix.systems?priority=7"
-      "https://nix-gaming.cachix.org?priority=8"
+      "https://nix-gaming.cachix.org?priority=7"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-      "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     ];
   };
@@ -174,7 +172,7 @@
 
             devshells.default = {
               packages = with pkgs; [
-                lix
+                nix
                 git
                 fish
                 sops
