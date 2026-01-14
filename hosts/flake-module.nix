0000280722405fb7651e931613e099a68b109d95
MCSeekeri <mcseekeri@outlook.com>
2026-01-14 20:00:55 +0800

    refactor(hosts): 重构 flake-module，不再需要手动配置主机列表

--- hosts/flake-module.nix
+++ hosts/flake-module.nix
@@ -1,25 +1,23 @@
 { inputs, lib, ... }:
-let
-  hostnames = [
-    "manhattan"
-    "seychelles"
-    "cyprus"
-    "cuba"
-    "costarica"
-  ];
-in
 {
   flake = {
-    nixosConfigurations = lib.genAttrs hostnames (
-      name:
-      inputs.nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit inputs;
-          inherit (inputs) self;
-        };
-        modules = [ ./${name} ];
-      }
-    );
+    nixosConfigurations =
+      lib.genAttrs
+        (builtins.attrNames (
+          lib.filterAttrs (name: type: type == "directory" && name != "flake-module.nix") (
+            builtins.readDir ./.
+          )
+        ))
+        (
+          name:
+          inputs.nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = {
+              inherit inputs;
+              inherit (inputs) self;
+            };
+            modules = [ ./${name} ];
+          }
+        );
   };
 }
