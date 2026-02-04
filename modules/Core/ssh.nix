0000287f064cf7de0bd286a214bc1cd3a6133eeb
MCSeekeri <mcseekeri@outlook.com>
2026-02-04 19:12:02 +0800

    feat: 实验性引入加固配置

--- modules/Core/ssh.nix
+++ modules/Core/ssh.nix
@@ -1,7 +1,17 @@
+{ lib, ... }:
 {
   services = {
     openssh = {
       enable = true;
+      settings = {
+        PermitEmptyPasswords = false;
+        X11Forwarding = lib.mkDefault false; # 真的有人还用这个？
+        ClientAliveInterval = 300;
+        ClientAliveCountMax = 0;
+        TCPKeepAlive = false;
+        # AllowTcpForwarding = false; # 禁止 TCP 转发，我偶尔从设备跳转到路由器的时候会用到……
+        AllowAgentForwarding = false;
+      };
       openFirewall = true;
       settings.PermitRootLogin = "no"; # 禁止 root 远程登录
       banner = "UNAUTHORIZED ACCESS TO THIS DEVICE IS PROHIBITED\n禁止未经授权访问本设备\n\nYou must have explicit, authorized permission to access or configure this device. Unauthorized attempts and actions to access or use this system may result in civil and/or criminal penalties. All activities performed on this device are logged and monitored.\n您必须具有明确的授权权限才能访问或配置此设备。未经授权尝试访问或使用本系统可能会导致民事和/或刑事处罚。在此设备上执行的所有活动都会被记录和监控。\n\n";
