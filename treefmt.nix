0000294e3074663c3b0f38e24f3c5323625fccd9
MCSeekeri <mcseekeri@outlook.com>
2026-02-09 19:06:36 +0800

    style: 完善代码格式化配置

--- treefmt.nix
+++ treefmt.nix
@@ -8,6 +8,33 @@
     };
     deadnix.enable = true;
     statix.enable = true;
+
+    dos2unix.enable = true;
+    actionlint.enable = true;
+    keep-sorted.enable = true;
+    autocorrect = {
+      enable = true;
+      settings = {
+        rules = {
+          space-word = "error";
+          space-punctuation = "error";
+          space-bracket = "error";
+          space-backticks = "error";
+          space-dash = "error";
+          space-dollar = "error";
+          fullwidth = "error";
+          no-space-fullwidth = "error";
+          halfwidth-word = "error";
+          halfwidth-punctuation = "error";
+          spellcheck = "error";
+        };
+        context.codeblock = "error";
+      };
+    };
+    sizelint = {
+      enable = true;
+      failOnWarn = true;
+    };
     mdformat = {
       enable = true;
       plugins = ps: [ ps.mdformat-gfm ];
