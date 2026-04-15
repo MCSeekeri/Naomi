0000349020f3531326e0b423593b3316b83fb3e4
MCSeekeri <mcseekeri@outlook.com>
2026-04-15 17:35:49 +0800

    refactor: dae 单独处理 DNS

--- modules/Services/dae/default.nix
+++ modules/Services/dae/default.nix
@@ -1,12 +1,21 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ lib, pkgs, ... }:
 {
   services.dae = {
     enable = true;
+    package = lib.mkDefault (
+      pkgs.dae.overrideAttrs {
+        # https://github.com/daeuniverse/flake.nix/blob/main/metadata.json
+        version = "unstable-2026-02-20.030902f";
+        src = pkgs.fetchFromGitHub {
+          owner = "daeuniverse";
+          repo = "dae";
+          rev = "030902f519f5b63f839327fd2fa9d8f906f4c504";
+          hash = "sha256-wIka/hzF2MzLebrgUHOB+BaRIEx4cD3TXPV9uqP9m7U=";
+          fetchSubmodules = true;
+        };
+        vendorHash = "sha256-MJVmAg+xyS53295FZ5HYB4rfbWHjUFBkxIOBxqxQP3U=";
+      }
+    );
     assets = with pkgs; [
       v2ray-geoip
       v2ray-domain-list-community
@@ -16,12 +25,9 @@
     config = lib.mkDefault (builtins.readFile ./config.dae);
   };
 
-  systemd.services.dae = lib.mkIf config.services.dae.enable {
-    after = lib.optional (config.services.adguardhome.enable or false) "adguardhome.service";
-    wants = lib.optional (config.services.adguardhome.enable or false) "adguardhome.service";
-    serviceConfig = {
-      Restart = "on-failure";
-      RestartSec = "5s";
-    };
+  networking = {
+    nameservers = lib.mkForce [ "127.0.0.1" ];
+    # 不幸的是，有些时候需要手动配置 NetworkManager 的 DNS 防止泄露
+    networkmanager.dns = lib.mkForce "none";
   };
 }
